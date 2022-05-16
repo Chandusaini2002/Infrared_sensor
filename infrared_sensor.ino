e47f7ebe7d13a26186d9c8a786ae2f6ad7de1b6e
Chandusaini2002 <98374607+Chandusaini2002@users.noreply.github.com>
2022-05-16 13:13:57 +0530

    Add files via upload

--- infrared_sensor.ino
+++ infrared_sensor.ino
@@ -1,11 +1,12 @@
-
+#include <Servo.h>
 int IRSensor = 2;
 int LED = 13; 
+Servo myservo;
 
 void setup() 
 {
 
-
+  myservo.attach(9);
 
   pinMode (IRSensor, INPUT); 
   pinMode (LED, OUTPUT); 
@@ -17,12 +18,18 @@ void loop()
   
   if (statusSensor == 1)
     {
-    digitalWrite(LED, LOW); 
+    digitalWrite(LED, HIGH); 
+    myservo.write(180);  
+        delay(1000);  
   }
   
   else
   {
-    digitalWrite(LED, HIGH);
+    digitalWrite(LED, LOW);
+       //delay(2000);
+       
+         myservo.write(90);
+        delay(2000);
   }
   
 }
